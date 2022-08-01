b582cf570cce97a2ab6a0912ddf8776de81a532d
Abdelrahman_Project <abdelrahman2490@yahoo.com>
2022-08-01 23:18:12 +0200

    Updatemaster

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import './i18n';
 
 ReactDOM.render(
   <React.StrictMode>
